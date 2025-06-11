adb198dac43ee9870684fa3021e8b45e1f3e8f06
iamharshtambe <harshtambe.dev@gmail.com>
2025-06-11 21:52:54 +0530

    added delete blog functionility

--- src/components/BlogList.jsx
+++ src/components/BlogList.jsx
@@ -1,10 +1,13 @@
-export function BlogList({ blogs }) {
+export function BlogList({ blogs, title, handleDelete }) {
    return (
       <div className="blog-list">
+         <h1>{title}</h1>
+
          {blogs.map((blog) => (
             <div className="blog-preview" key={blog.id}>
                <h2>{blog.title} </h2>
                <p>{blog.author}</p>
+               <button onClick={() => handleDelete(blog.id)}>Delete</button>
             </div>
          ))}
       </div>
