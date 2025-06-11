f65f82629d432b4e2d9dd6959ebd101856a3b75b
iamharshtambe <harshtambe.dev@gmail.com>
2025-06-11 21:37:31 +0530

    made BlogList component

---
+++ src/components/BlogList.jsx
@@ -0,0 +1,12 @@
+export function BlogList({ blogs }) {
+   return (
+      <div className="blog-list">
+         {blogs.map((blog) => (
+            <div className="blog-preview" key={blog.id}>
+               <h2>{blog.title} </h2>
+               <p>{blog.author}</p>
+            </div>
+         ))}
+      </div>
+   );
+}
