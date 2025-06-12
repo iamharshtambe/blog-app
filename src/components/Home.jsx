78c5b7f861ebf9492263abc80d0eff1de5f68ce4
iamharshtambe <harshtambe.dev@gmail.com>
2025-06-12 18:34:21 +0530

    added custom hook

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,32 +1,13 @@
 import { useEffect, useState } from 'react';
 import { BlogList } from './BlogList';
+import { useFetch } from '../hooks/useFetch';
 
 export function Home() {
-   const [blogs, setBlogs] = useState(null);
-   const [isLoading, setIsLoading] = useState(true);
-   const [error, setError] = useState(null);
-
-   useEffect(() => {
-      setTimeout(() => {
-         fetch('http://localhost:8000/blogs')
-            .then((response) => {
-               if (!response.ok) {
-                  throw Error('Could not fetch the data from that resource');
-               }
-
-               return response.json();
-            })
-            .then((data) => {
-               setBlogs(data);
-               setIsLoading(false);
-               setError(null);
-            })
-            .catch((err) => {
-               setError(err.message);
-               setIsLoading(false);
-            });
-      }, 1000);
-   }, []);
+   const {
+      data: blogs,
+      isLoading,
+      error,
+   } = useFetch('http://localhost:8000/blogs');
 
    return (
       <div className="home">
---
+++ src/hooks/useFetch.js
@@ -0,0 +1,31 @@
+import { useState, useEffect } from 'react';
+
+export function useFetch(url) {
+   const [data, setData] = useState(null);
+   const [isLoading, setIsLoading] = useState(true);
+   const [error, setError] = useState(null);
+
+   useEffect(() => {
+      setTimeout(() => {
+         fetch(url)
+            .then((response) => {
+               if (!response.ok) {
+                  throw Error('Could not fetch the data from that resource');
+               }
+
+               return response.json();
+            })
+            .then((data) => {
+               setData(data);
+               setIsLoading(false);
+               setError(null);
+            })
+            .catch((err) => {
+               setError(err.message);
+               setIsLoading(false);
+            });
+      }, 1000);
+   }, []);
+
+   return { data, isLoading, error };
+}
