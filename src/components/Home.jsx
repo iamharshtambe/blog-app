61225d4588e81c67d22e8e44b0898a3cbf43bd51
iamharshtambe <harshtambe.dev@gmail.com>
2025-06-12 18:00:39 +0530

    added error handling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,15 +4,25 @@ import { BlogList } from './BlogList';
 export function Home() {
    const [blogs, setBlogs] = useState(null);
    const [isLoading, setIsLoading] = useState(true);
+   const [error, setError] = useState(null);
 
    useEffect(() => {
       setTimeout(() => {
          fetch('http://localhost:8000/blogs')
             .then((response) => {
+               if (!response.ok) {
+                  throw Error('Could not fetch the data from that resource');
+               }
+
                return response.json();
             })
             .then((data) => {
                setBlogs(data);
+               setIsLoading(false);
+               setError(null);
+            })
+            .catch((err) => {
+               setError(err.message);
                setIsLoading(false);
             });
       }, 1000);
@@ -20,6 +30,7 @@ export function Home() {
 
    return (
       <div className="home">
+         {error && <div>{error}</div>}
          {isLoading && <div>Loading...</div>}
          {blogs && <BlogList blogs={blogs} title={'All Blogs!'} />}
       </div>
